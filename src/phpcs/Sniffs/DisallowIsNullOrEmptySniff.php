9cc2cb54be0e317f46f8fd4ccceda1a9749c171e
noko206 <61347127+noko206@users.noreply.github.com>
2023-07-19 09:53:56 +0900

    add: PHP CodeSnifferのテスト

--- src/phpcs/Sniffs/DisallowIsNullOrEmptySniff.php
+++ src/phpcs/Sniffs/DisallowIsNullOrEmptySniff.php
@@ -5,7 +5,7 @@ namespace Wim\Sniffs\ArrowFunction;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
 
-class CoreTechSniff implements Sniff
+class DisallowIsNullOrEmptySniff implements Sniff
 {
 	public function register()
 	{
@@ -15,13 +15,19 @@ class CoreTechSniff implements Sniff
 	public function process(File $phpcsFile, $stackPtr)
 	{
 		$tokens = $phpcsFile->getTokens();
-		if ($tokens[$stackPtr]['content'] === 'Test' && $tokens[$stackPtr + 1]['content'] === '::' && $tokens[$stackPtr + 2]['content'] === 'isNullOrEmpty') {
+
+		$content1 = $tokens[$stackPtr]['content'];
+		$content2 = $tokens[$stackPtr + 1]['content'] ?? '';
+		$content3 = $tokens[$stackPtr + 2]['content'] ?? '';
+
+		if ($content1 === 'Func_App' && $content2 === '::' && $content3 === 'isNullOrEmpty') {
 			$error = 'isNullOrEmpty関数は使用しないでください。';
 			$fix = $phpcsFile->addFixableError($error, $stackPtr, 'Found');
+
 			if ($fix) {
-				$phpcsFile->fixer->replaceToken($stackPtr, '');
+				$phpcsFile->fixer->replaceToken($stackPtr, 'empty');
 				$phpcsFile->fixer->replaceToken($stackPtr + 1, '');
-				$phpcsFile->fixer->replaceToken($stackPtr + 2, 'empty');
+				$phpcsFile->fixer->replaceToken($stackPtr + 2, '');
 			}
 		}
 	}
